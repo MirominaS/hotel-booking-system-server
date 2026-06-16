620f868313d53bdbe32ab49d6fab202b37f1c371
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-16 17:24:11 +0530

    refactor app.js to organize route imports and middleware usage

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@ import express from "express";
 import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
+import hotelRoutes from "./routes/hotelRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import customerRoutes from "./routes/customerRoutes.js"
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
 //auth
 app.use("/api/auth", authRoutes);
 
-app.get("/",(req, res) => {
-    res.send("Server is running")
-})
+//hotel-owner
+app.use("/api/hotel-owner", hotelRoutes);
+
+//admin
+app.use("/api/admin", adminRoutes);
+
+//customer
+app.use("/api/customer", customerRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 export default app;
