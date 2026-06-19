33733dee2aad2ffcc4509c5f7dbb72b1de1842ea
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-19 21:56:22 +0530

    style(app.js): format import statements and ensure consistent semicolon usage

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import hotelRoutes from "./routes/hotelRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import customerRoutes from "./routes/customerRoutes.js"
-import roomRoutes from "./routes/roomRoutes.js"
-import roomTypeRoutes from "./routes/roomTypeRoutes.js"
-import bookingRoutes from "./routes/bookingRoutes.js"
-import paymentRoutes from "./routes/paymentRoutes.js"
-import mediaRoutes from "./routes/mediaRoutes.js"
-import hotelOwnerRoutes from "./routes/hotelOwnerRoutes.js"
-import hotelAnalyticsRoutes from "./routes/hotelAnalyticsRoutes.js"
+import customerRoutes from "./routes/customerRoutes.js";
+import roomRoutes from "./routes/roomRoutes.js";
+import roomTypeRoutes from "./routes/roomTypeRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import mediaRoutes from "./routes/mediaRoutes.js";
+import hotelOwnerRoutes from "./routes/hotelOwnerRoutes.js";
+import hotelAnalyticsRoutes from "./routes/hotelAnalyticsRoutes.js";
+import adminAnalyticsRoutes from "./routes/adminAnalyticsRoutes.js";
 
 const app = express();
 app.use(cors());
@@ -23,28 +24,29 @@ app.use("/api/auth", authRoutes);
 
 //hotel-owner
 app.use("/api/hotel", hotelRoutes);
-app.use("/api/hotel-owner", hotelOwnerRoutes)
+app.use("/api/hotel-owner", hotelOwnerRoutes);
 
 //hotel-room
-app.use("/api/rooms", roomRoutes)
-app.use("/api/room-types", roomTypeRoutes)
+app.use("/api/rooms", roomRoutes);
+app.use("/api/room-types", roomTypeRoutes);
 
-app.use("/api/booking", bookingRoutes)
+app.use("/api/booking", bookingRoutes);
 
 //hotel-analytics
-app.use("/api/hotel-analytics", hotelAnalyticsRoutes)
+app.use("/api/hotel-analytics", hotelAnalyticsRoutes);
 
 //admin
 app.use("/api/admin", adminRoutes);
+app.use("/api/admin-analytics", adminAnalyticsRoutes);
 
 //customer
-app.use("/api/customer", customerRoutes)
+app.use("/api/customer", customerRoutes);
 
 //payment
-app.use("/api/payments", paymentRoutes)
+app.use("/api/payments", paymentRoutes);
 
 //media
-app.use("/api/media", mediaRoutes)
+app.use("/api/media", mediaRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
