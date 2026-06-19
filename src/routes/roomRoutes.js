0c97d356bfff2adf45fa115f119a3c21239ddb96
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-19 13:47:45 +0530

    add(roomRoutes): implement update and delete routes for hotel rooms

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -6,6 +6,8 @@ import {
   createRoom,
   getHotelRooms,
   getRoomById,
+  updateRoom,
+  deleteRoom,
 } from "../controllers/roomController.js";
 
 const router = express.Router();
@@ -13,5 +15,7 @@ const router = express.Router();
 router.post("/:hotelId", protect, authorize("hotel_owner"), createRoom);
 router.get("/hotel/:hotelId", protect, authorize("hotel_owner"), getHotelRooms);
 router.get("/:id", protect, authorize("hotel_owner"), getRoomById);
+router.put("/:hotelId/rooms/:roomId",protect, authorize("hotel_owner"), updateRoom,);
+router.delete( "/:hotelId/rooms/:roomId",  protect, authorize("hotel_owner"), deleteRoom,);
 
 export default router;
